998b627eae830b5f70be8a5c6db942fabee2711c
Hugo Salles <salles.hugo05@gmail.com>
2022-08-29 10:55:21 -0400

    creating backend and deploying to firebase

---
+++ mechConnect-api/functions/index.js
@@ -0,0 +1,24 @@
+import functions from 'firebase-functions'
+import express from 'express'
+import cors from 'cors'
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/test', (req, res) => {
+    res.send('Hosting succesful')
+})
+
+
+
+export const api = functions.https.onRequest(app);
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
